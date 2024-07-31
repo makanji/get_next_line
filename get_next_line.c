05b789d32d313285da26393d7d7b4ad86e003e8b
makanji <hardeyqiunle@gmail.com>
2024-07-31 13:41:27 +0200

    finished get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -1,19 +1,129 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: makanji <makanji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 22:37:53 by makanji           #+#    #+#             */
-/*   Updated: 2024/07/25 16:29:34 by makanji          ###   ########.fr       */
+/*   Updated: 2024/07/31 12:18:48 by makanji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// #include "get_next_line.h"
+
+// #ifndef BUFFER_SIZE
+// #define	BUFFER_SIZE 102
+// #endif
+
+// static char	*expand_buffer(char *buffer, char *temp)
+// {
+// 	char	*new_buffer;
+
+// 	if (!buffer)
+// 		return (ft_strdup(temp));
+// 	new_buffer = ft_strjoin(buffer, temp);
+// 	free(buffer);
+// 	return (new_buffer);
+// }
+
+// static char	*read_file(int fd, char *buffer)
+// {
+// 	char	*temp;
+// 	ssize_t	bytes_read;
+
+// 	temp = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+// 	if (!temp)
+// 		return (NULL);
+// 	bytes_read = 1;
+// 	while (bytes_read > 0 && (!buffer || !ft_strchr(buffer, '\n')))
+// 	{
+// 		bytes_read = read(fd, temp, BUFFER_SIZE);
+// 		if (bytes_read == -1)
+// 		{
+// 			free(temp);
+// 			return (NULL);
+// 		}
+// 		temp[bytes_read] = '\0';
+// 		buffer = expand_buffer(buffer, temp);
+// 		if (!buffer)
+// 		{
+// 			free(temp);
+// 			return (NULL);
+// 		}
+// 	}
+// 	free(temp);
+// 	return (buffer);
+// }
+
+// static char	*extract_line(char *buffer)
+// {
+// 	char	*line;
+// 	int		i;
+
+// 	if (!buffer[0])
+// 		return (NULL);
+// 	i = 0;
+// 	while (buffer[i] && buffer[i] != '\n')
+// 		i++;
+// 	line = ft_calloc(i + 2, sizeof(char));
+// 	if (!line)
+// 		return (NULL);
+// 	i = 0;
+// 	while (buffer[i] && buffer[i] != '\n')
+// 	{
+// 		line[i] = buffer[i];
+// 		i++;
+// 	}
+// 	if (buffer[i] == '\n')
+// 		line[i] = '\n';
+// 	return (line);
+// }
+
+// static char	*update_buffer(char *buffer)
+// {
+// 	char	*new_buffer;
+// 	int		i;
+// 	int		j;
+
+// 	i = 0;
+// 	while (buffer[i] && buffer[i] != '\n')
+// 		i++;
+// 	if (!buffer[i])
+// 	{
+// 		free(buffer);
+// 		return (NULL);
+// 	}
+// 	new_buffer = ft_calloc(ft_strlen(buffer) - i + 1, sizeof(char));
+// 	if (!new_buffer)
+// 		return (NULL);
+// 	i++;
+// 	j = 0;
+// 	while (buffer[i])
+// 		new_buffer[j++] = buffer[i++];
+// 	free(buffer);
+// 	return (new_buffer);
+// }
+
+// char	*get_next_line(int fd)
+// {
+// 	static char	*buffer;
+// 	char		*line;
+
+// 	if (fd < 0 || BUFFER_SIZE <= 0)
+// 		return (NULL);
+// 	buffer = read_file(fd, buffer);
+// 	if (!buffer)
+// 		return (NULL);
+// 	line = extract_line(buffer);
+// 	buffer = update_buffer(buffer);
+// 	return (line);
+// }
 
 #include "get_next_line.h"
 
 #ifndef BUFFER_SIZE
-#define	BUFFER_SIZE 102
+# define BUFFER_SIZE 102
 #endif
 
 static char	*expand_buffer(char *buffer, char *temp)
@@ -36,24 +146,19 @@ static char	*read_file(int fd, char *buffer)
 	if (!temp)
 		return (NULL);
 	bytes_read = 1;
-	while (bytes_read > 0 && (!buffer || !ft_strchr(buffer, '\n')))
+	while (bytes_read > 0)
 	{
 		bytes_read = read(fd, temp, BUFFER_SIZE);
 		if (bytes_read == -1)
-		{
-			free(temp);
-			return (NULL);
-		}
+			return (free(temp), free(buffer), NULL);
 		temp[bytes_read] = '\0';
 		buffer = expand_buffer(buffer, temp);
 		if (!buffer)
-		{
-			free(temp);
-			return (NULL);
-		}
+			return (free(temp), NULL);
+		if (ft_strchr(buffer, '\n'))
+			break ;
 	}
-	free(temp);
-	return (buffer);
+	return (free(temp), buffer);
 }
 
 static char	*extract_line(char *buffer)
@@ -61,7 +166,7 @@ static char	*extract_line(char *buffer)
 	char	*line;
 	int		i;
 
-	if (!buffer[0])
+	if (!buffer || !buffer[0])
 		return (NULL);
 	i = 0;
 	while (buffer[i] && buffer[i] != '\n')
@@ -90,19 +195,19 @@ static char	*update_buffer(char *buffer)
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
 	if (!buffer[i])
-	{
-		free(buffer);
-		return (NULL);
-	}
+		return (free(buffer), NULL);
 	new_buffer = ft_calloc(ft_strlen(buffer) - i + 1, sizeof(char));
 	if (!new_buffer)
-		return (NULL);
+		return (free(buffer), NULL);
 	i++;
 	j = 0;
 	while (buffer[i])
-		new_buffer[j++] = buffer[i++];
-	free(buffer);
-	return (new_buffer);
+	{
+		new_buffer[j] = buffer[i];
+		i++;
+		j++;
+	}
+	return (free(buffer), new_buffer);
 }
 
 char	*get_next_line(int fd)
--- get_next_line.h
+++ get_next_line.h
@@ -1,14 +1,14 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        :::      ::::::::   */
-// /*   get_next_line.h                                    :+:      :+:    :+:   */
-// /*                                                    +:+ +:+         +:+     */
-// /*   By: makanji <makanji@student.42.fr>            +#+  +:+       +#+        */
-// /*                                                +#+#+#+#+#+   +#+           */
-// /*   Created: 2024/07/18 22:37:53 by makanji           #+#    #+#             */
-// /*   Updated: 2024/07/25 16:29:34 by makanji          ###   ########.fr       */
-// /*                                                                            */
-// /* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makanji <makanji@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 22:37:53 by makanji           #+#    #+#             */
+/*   Updated: 2024/07/31 12:23:10 by makanji          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
@@ -18,11 +18,11 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-char    *get_next_line(int fd);
-char    *ft_strjoin(char const *s1, char const *s2);
-char    *ft_strchr(const char *str, int char_checked);
-void    *ft_calloc(size_t count, size_t size);
-size_t  ft_strlen(const char *string);
-char    *ft_strdup(const char *s1);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *str, int char_checked);
+void	*ft_calloc(size_t count, size_t size);
+size_t	ft_strlen(const char *string);
+char	*ft_strdup(const char *s1);
 
 #endif
