82cd3e1cfd285da9f8c66a3892ce7bda38769096
makanji <hardeyqiunle@gmail.com>
2024-08-02 00:46:59 +0200

    main for check

--- main.c
+++ main.c
@@ -1,20 +1,43 @@
+// #include "get_next_line.h"
+// int main()
+// {
+//     int fd = open("test.txt", O_RDWR);
+//     if (fd == -1)
+//     {
+//         perror("open");
+//         return 1;
+//     }
+//     char *str;
+//     str = get_next_line(fd);
+//     while ((str = get_next_line(fd)) != NULL)
+//     {
+//         printf("%s\n", str);
+//         free(str);
+//     }
+//     free(str);
+//     close(fd);
+//     return 0;
+// }
+
 #include "get_next_line.h"
-int main()
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(void)
 {
-    int fd = open("test.txt", O_RDWR);
+    int fd = open("test.txt", O_RDONLY);
+    char *line;
+
     if (fd == -1)
     {
-        perror("open");
-        return 1;
+        perror("Error opening file");
+        return (1);
     }
-    char *str;
-    str = get_next_line(fd);
-    while ((str = get_next_line(fd)) != NULL)
+    while ((line = get_next_line(fd)) != NULL)
     {
-        printf("%s\n", str);
-        free(str);
+        printf("%s", line);
+        free(line);
     }
-    free(str);
     close(fd);
-    return 0;
+    return (0);
 }
